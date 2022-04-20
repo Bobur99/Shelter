0fff74493973a56464965602a20d8bf28dfe1780
coder <coder>
2022-04-20 21:42:51 +0500

    configure slider

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
 window.onload = function () {
-  // $(".mySliderPets").slick({
-  //   slidesToShow: 3,
-  //   slidesToScroll: 1,
-  //   infinite: false,
-  //   autoplay: false,
-  //   autoplaySpeed: 2000,
-  //   responsive: [
-  //     {
-  //       breakpoint: 992,
-  //       settings: {
-  //         slidesToShow: 2,
-  //       },
-  //     },
-  //     {
-  //       breakpoint: 650,
-  //       settings: {
-  //         slidesToShow: 1,
-  //       },
-  //     },
-  //   ],
-  // });
-
-  const swiper = new Swiper('.swiper', {
-    slidesPerView: 3,
-    spaceBetween: 30,
+  const swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    breakpoints: {
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      650: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      1190: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+    },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
@@ -34,28 +26,25 @@ window.onload = function () {
     },
   });
 
-  
-  const menuBtn = document.querySelector('.header__menu_btn')
-  const menuBtnIcon = document.querySelector('.header__menu_btn i')
-  const menuList = document.querySelector('.nav__list')
+  const menuBtn = document.querySelector(".header__menu_btn");
+  const menuBtnIcon = document.querySelector(".header__menu_btn i");
+  const menuList = document.querySelector(".nav__list");
 
-  menuBtn.addEventListener('click', function () {
-    menuList.classList.toggle('active')
-    menuBtnIcon.classList.toggle('active')
-  })
+  menuBtn.addEventListener("click", function () {
+    menuList.classList.toggle("active");
+    menuBtnIcon.classList.toggle("active");
+  });
 
-  const navLinks = document.querySelectorAll('.nav__list li a')
+  const navLinks = document.querySelectorAll(".nav__list li a");
 
-    for(let i = 0; i < navLinks.length; i++){
-      navLinks[i].addEventListener('click', function() {
-        navLinks.forEach(link => {
-          link.classList.remove('active')
-        })
-        if(!navLinks[i].classList.contains('active')){
-          navLinks[i].classList.add('active')
-        } 
-      })
-    }
-  
-
-}
+  for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener("click", function () {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+      });
+      if (!navLinks[i].classList.contains("active")) {
+        navLinks[i].classList.add("active");
+      }
+    });
+  }
+};
